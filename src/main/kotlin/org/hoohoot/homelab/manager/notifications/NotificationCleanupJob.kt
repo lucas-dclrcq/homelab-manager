c48f9cde3fe8cf383f792a9e2ab135ef3b9fbe82
Lucas Declercq <lucas.declercq@hoohoot.org>
2026-03-26 09:27:42 +0100

    fix: enable context on scheduled jobs

--- src/main/kotlin/org/hoohoot/homelab/manager/notifications/NotificationCleanupJob.kt
+++ src/main/kotlin/org/hoohoot/homelab/manager/notifications/NotificationCleanupJob.kt
@@ -3,12 +3,14 @@ package org.hoohoot.homelab.manager.notifications
 import io.quarkus.logging.Log
 import io.quarkus.scheduler.Scheduled
 import jakarta.enterprise.context.ApplicationScoped
+import jakarta.enterprise.context.control.ActivateRequestContext
 import org.hoohoot.homelab.manager.notifications.persistence.NotificationSentRepository
 
 @ApplicationScoped
 class NotificationCleanupJob(private val repo: NotificationSentRepository) {
 
     @Scheduled(every = "15m")
+    @ActivateRequestContext
     suspend fun cleanupExpiredThreads() {
         Log.info("Running cleanup of expired media notification threads")
         repo.deleteExpiredThreads()
--- src/main/kotlin/org/hoohoot/homelab/manager/notifications/weeklyreport/WeeklyReportJob.kt
+++ src/main/kotlin/org/hoohoot/homelab/manager/notifications/weeklyreport/WeeklyReportJob.kt
@@ -3,10 +3,12 @@ package org.hoohoot.homelab.manager.notifications.weeklyreport
 import io.quarkus.logging.Log
 import io.quarkus.scheduler.Scheduled
 import jakarta.enterprise.context.ApplicationScoped
+import jakarta.enterprise.context.control.ActivateRequestContext
 
 @ApplicationScoped
 class WeeklyReportJob(private val weeklyReportService: WeeklyReportService) {
     @Scheduled(cron = "{weekly-report.cron}")
+    @ActivateRequestContext
     suspend fun sendWeeklyReport() {
         Log.info("Scheduled weekly report job triggered")
         weeklyReportService.sendWeeklyReport()
